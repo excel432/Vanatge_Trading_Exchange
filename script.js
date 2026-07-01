e10fe6455758cfced99e0c3ac87336c19fe230a0
excel432 <exell4321@gmail.com>
2026-07-01 12:57:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleChat() {
+  var box = document.getElementById('chatBox');
+  if (box.style.display === 'block') {
+    box.style.display = 'none';
+  } else {
+    box.style.display = 'block';
+  }
+}
+
+function sendMessage() {
+  var input = document.getElementById('userInput');
+  var messages = document.getElementById('chatMessages');
+  var userText = input.value;
+  if (userText === '') return;
+  messages.innerHTML += '<div class="user-msg">' + userText + '</div>';
+  var reply = getReply(userText.toLowerCase());
+  messages.innerHTML += '<div class="bot-msg">' + reply + '</div>';
+  input.value = '';
+  messages.scrollTop = messages.scrollHeight;
+}
+
+function getReply(msg) {
+  if (msg.indexOf('forex') !== -1) {
+    return 'We offer forex trading on all major pairs with tight spreads!';
+  } else if (msg.indexOf('crypto') !== -1) {
+    return 'Trade Bitcoin, Ethereum and top altcoins 24/7 on Kendit!';
+  } else if (msg.indexOf('gold') !== -1) {
+    return 'We offer gold, oil and silver commodities trading!';
+  } else if (msg.indexOf('account') !== -1) {
+    return 'Fill out the contact form and we will reach out within 24 hours!';
+  } else {
+    return 'Thanks! Please fill out our contact form and we will get back to you.';
+  }
+}
+
+
+
